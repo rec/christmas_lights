954763750f648351bd7034e24224b9ad4479a440
Tom Ritchford <tom@swirly.com>
2017-12-15 17:27:42 +0100

    inc

--- christmas_lights/light2.py
+++ christmas_lights/light2.py
@@ -17,14 +17,14 @@ class Sprite1d:
         self.center = int(len(self.icon) / 2) if center is None else center
         self.fps = 0
 
-    def render(self):
-        whole, fraction = divmod(self.position * len(self.icon), 1)
-        left = int(whole) - self.center
-        right = left + len(self.icon)
+    def _display(self):
+        # Handle subpixel positioning.
+        whole, fraction = divmod(self.position * len(self.color_list), 1)
+        left, right = int(whole) - self.radius, int(whole) + self.radius
 
-        self._combine_clipped(left, right, 1 - fraction)
+        self._add(left, right, 1 - fraction)
         if fraction:
-            self._combine_clipped(left + 1, right + 1, fraction)
+            self._add(left + 1, right + 1, fraction)
 
     def _combine_numpy(self, left, right, ratio, pixels):
         self.color_list[left:right] += ratio * pixels
@@ -53,6 +53,18 @@ class Sprite1d:
             right = len(self.icon) - 1
 
         self._combine(left, right, ratio, pixels)
+
+    def _move(self, amt):
+        self.position += amt * self.speed / self.fps
+
+    def _bounce(self):
+        left, right = self.bound
+        if self.position < left and self.speed < 0:
+            self.position = left + (left - self.position)
+            self.speed = -self.speed
+        if self.position >= right and self.speed > 0:
+            self.position = right - (self.position - right)
+            self.speed = -self.speed
 
     def _add(self, left, right, ratio):
         # print('add', left, right, ratio)
@@ -96,25 +108,7 @@ class Light(Sprite1d):
         super().__init__(
             icon, color_list, number(speed), bound, number(position))
 
-    def _display(self):
-        # Handle subpixel positioning.
-        whole, fraction = divmod(self.position * len(self.color_list), 1)
-        left, right = int(whole) - self.radius, int(whole) + self.radius
-
-        self._add(left, right, 1 - fraction)
-        if fraction:
-            self._add(left + 1, right + 1, fraction)
-
-    def _move(self, amt):
-        self.position += amt * self.speed / self.fps
-        left, right = self.bound
-        if self.position < left and self.speed < 0:
-            self.position = left + (left - self.position)
-            self.speed = -self.speed
-        if self.position >= right and self.speed > 0:
-            self.position = right - (self.position - right)
-            self.speed = -self.speed
-
     def step(self, amt):
         self._display()
         self._move(amt)
+        self._bounce()
